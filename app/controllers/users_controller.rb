0e459e8a5ce67bdfe83d1bd04278b6189ae35085
Benshalom <benshalom.kariuki@kopokopo.com>
2022-02-11 21:18:39 +0300

    Add remove friend tracking functionality, create friendship tracking from browserr, add show user/friend profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,21 @@
 class UsersController < ApplicationController
+
   def my_portfolio
      @tracked_stocks = current_user.stocks
   end
 
-  def my_friends
-@friends = current_user.friends
-  end
+
+def my_friends
+    @friends = current_user.friends
+end
 
 
-def search
+def search 
+
     if params[:friend].present?
-    @friend = params[:friend]
-    if @friend
+    @friends =  User.search(params[:friend])
+    @friends = current_user.except_current_user(@friends)
+    if @friends
     respond_to do |format|
     format.js { render partial: 'users/friend_result' }
     end 
